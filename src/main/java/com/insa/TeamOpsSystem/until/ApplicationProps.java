0d586f80fddb600d130e1bcbcf45aadd18f39e0e
unknown <mazengiya.tesfa@enatbanksc.com>
2024-04-16 09:06:30 +0300

    fix security config

---
+++ src/main/java/com/insa/TeamOpsSystem/until/ApplicationProps.java
@@ -0,0 +1,19 @@
+package com.insa.TeamOpsSystem.until;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@ConfigurationProperties(prefix = "application")
+@Data
+@Primary
+public class ApplicationProps {
+    private List<String> allowedOrigin;
+}
+
+
+
