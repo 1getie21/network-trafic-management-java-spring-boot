848097020340aadfe2c97da1314a5e0c4b74628d
1getie21 <1getie21@example.com>
2024-06-06 08:45:20 +0300

    fix comment

--- src/main/java/com/insa/TeamOpsSystem/request/RequestService.java
+++ src/main/java/com/insa/TeamOpsSystem/request/RequestService.java
@@ -49,8 +49,7 @@ public class RequestService {
         var et = getTrafficById(id);
 
         BeanUtils.copyProperties(request, et, getNullPropertyNames(request));
-
-
+        et.setStatus("Pending");
         return requestRepository.save(et);
 
     }
